acf251dc1c98e77c288a70ce379d329960441c8b
hector perez <hectorperez1238@gmail.com>
2021-12-21 15:34:32 -0400

    agregados los includes

---
+++ view/contacto.php
@@ -0,0 +1,62 @@
+<?php include("../includes/header.php")  ?>
+<?php include("../includes/menu_navegacion.php")  ?>
+
+
+
+<div class="header__content">
+	<img src="../assets/img/Seccion 01.png" alt="" class="header__img" />
+</div>
+
+
+</div>
+
+<section class="contactanos__container">
+	<div class="contactanos__form">
+		<h3 class="contactanos__title">DEJANOS UN MENSAJE</h3>
+		<form action="">
+			<input type="text" placeholder="Nombre" />
+			<input type="email" placeholder="Email" />
+			<input type="number" placeholder="Telefono" />
+			<input type="Asunto" placeholder="Asunto" />
+			<textarea name="mensaje" rows="10" placeholder="Mensaje"></textarea>
+			<button class="contactos__btn">ENVIAR</button>
+		</form>
+	</div>
+
+	<div class="contactanos__info">
+		<h3>CONTACTANOS</h3>
+		<div class="contactanos__row-1">
+			En caso de estar interesado/a en uno de nuestros productos, te
+			solicitamos que nos dejes un numero de contacto para que pueda
+			comunicarse un asesor
+		</div>
+		<div class="contactanos__row-2">
+			Para conocer el estado de tu Plan, envíanos un WhatsApp al 3513664915
+			y un representante de adminitración te adjuntará la información
+			solicitada.
+		</div>
+
+		<div class="contactanos__row-3">
+			<ul>
+				<li>
+					<i class="bx bxs-map"></i>
+					<small>Av. Marcelo T. de Alvear 750</small>
+				</li>
+				<li>
+					<i class="bx bx-mobile-alt"></i>
+					<small>+54 0351 342-1277</small>
+				</li>
+				<li>
+					<i class="bx bx-mail-send"></i>
+					<small>genesismotorsadm@gmail.com</small>
+				</li>
+			</ul>
+		</div>
+	</div>
+</section>
+
+<section class="mapa_-negocio">
+	<iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3404.6682239738398!2d-64.1956542848518!3d-31.423265881401772!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x9432998065cef88f%3A0xb575444e9a623c31!2sG%C3%A9nesis%20motors!5e0!3m2!1ses-419!2sve!4v1639159298909!5m2!1ses-419!2sve" width="100%" height="450" style="border: 0" allowfullscreen="" loading="lazy"></iframe>
+</section>
+
+<?php include("../includes/footer.php")  ?>
---
+++ view/enduro.php
@@ -0,0 +1,96 @@
+<?php include("../includes/header.php")  ?>
+<?php include("../includes/menu_navegacion.php")  ?>
+
+<div class="header__content">
+	<img src="../assets/img/Seccion 2.png" alt="" class="header__img" />
+</div>
+</div>
+
+<section class="scooter__product">
+	<div class="scooter__categoria">
+		<div class="scooter__container">
+			<div class="scooter__header">
+				<h2>CATEGORÍAS</h2>
+			</div>
+			<div class="cantegoria__content">
+				<a href="/view/scooter.php">
+					<h2>CUB/SCOOTERS</h2>
+				</a>
+				<hr />
+				<a href="/view/nake_sport.php">
+					<h2>NAKED/SPORT</h2>
+				</a>
+				<hr />
+				<a href="/view/enduro.php" class="active">
+					<h2>ENDURO</h2>
+				</a>
+			</div>
+		</div>
+	</div>
+
+	<div class="product__list">
+		<div class="product__item">
+			<div class="item__img">
+				<img src="/assets/img/IMAGEN 01_enduro.png" alt="" />
+			</div>
+			<div class="item__container">
+				<div class="item__title">
+					<h2>HONDA XR 150L</h2>
+					<p>Cilindraje: 150cc</p>
+				</div>
+				<a href="/view/product.php" class="btn__detalles">
+					<i class="bx bx-chevron-right"></i>
+					<p>VER DETALLES</p>
+				</a>
+			</div>
+		</div>
+		<div class="product__item">
+			<div class="item__img">
+				<img src="/assets/img/IMAGEN 02_enduro.png" alt="" />
+			</div>
+			<div class="item__container">
+				<div class="item__title">
+					<h2>HONDA XR 250 TORNADO</h2>
+					<p>Cilindraje: 250cc</p>
+				</div>
+				<a href="/view/product.php" class="btn__detalles">
+					<i class="bx bx-chevron-right"></i>
+					<p>VER DETALLES</p>
+				</a>
+			</div>
+		</div>
+		<div class="product__item">
+			<div class="item__img">
+				<img src="/assets/img/IMAGEN 02 (1)_enduro.png" alt="" />
+			</div>
+			<div class="item__container">
+				<div class="item__title">
+					<h2>YAMAHA XTZ125</h2>
+					<p>Cilindraje: 125cc</p>
+				</div>
+				<a href="/view/product.php" class="btn__detalles">
+					<i class="bx bx-chevron-right"></i>
+					<p>VER DETALLES</p>
+				</a>
+			</div>
+		</div>
+
+		<div class="product__item">
+			<div class="item__img">
+				<img src="/assets/img/IMAGEN 02 (2)_enduro.png" alt="" />
+			</div>
+			<div class="item__container">
+				<div class="item__title">
+					<h2>YAMAHA XTZ150</h2>
+					<p>Cilindraje: 150cc</p>
+				</div>
+				<a href="/view/product.php" class="btn__detalles">
+					<i class="bx bx-chevron-right"></i>
+					<p>VER DETALLES</p>
+				</a>
+			</div>
+		</div>
+	</div>
+</section>
+
+<?php include("../includes/footer.php")  ?>
