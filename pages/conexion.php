a6966fbf1c2d5f3799617d7f586aee28fd9578f3
maticab88 <83186721+maticab88@users.noreply.github.com>
2022-03-10 20:19:00 -0300

    Update conexion.php

--- pages/conexion.php
+++ pages/conexion.php
@@ -1,5 +1,5 @@
 <?php
-$conexion = mysqli_connect("dcrhg4kh56j13bnu.cbetxkdyhwsb.us-east-1.rds.amazonaws.com", "ycxnuwhxu1mmnnkf", "c0jppu3fs57blhf1", "alcmzlhzcq0iqmyq");
+$conexion = mysqli_connect("dcrhg4kh56j13bnu.cbetxkdyhwsb.us-east-1.rds.amazonaws.com", "md6fd8nkq8rnulq9", "ftg7co6juahp0yld", "nld53pd37s54moii");
 if(mysqli_connect_error()){
     echo "La conexión a la base de datos sql ha fallado: ".mysqli_connect_error();
 }else{
@@ -9,7 +9,7 @@ if(mysqli_connect_error()){
 mysqli_query($conexion, "SET NAMES 'utf8'");
 
 
-/*$conexion = mysqli_connect("dcrhg4kh56j13bnu.cbetxkdyhwsb.us-east-1.rds.amazonaws.com", "ycxnuwhxu1mmnnkf", "c0jppu3fs57blhf1", "alcmzlhzcq0iqmyq");
+/*$conexion = mysqli_connect("dcrhg4kh56j13bnu.cbetxkdyhwsb.us-east-1.rds.amazonaws.com", "md6fd8nkq8rnulq9", "ftg7co6juahp0yld", "nld53pd37s54moii");
 if(mysqli_connect_error()){
     echo "La conexión a la base de datos sql ha fallado: ".mysqli_connect_error();
 }else{
