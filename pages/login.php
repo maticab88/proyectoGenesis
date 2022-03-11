d0d2c326295ab1548f0f3f727d8f7290f422ddfe
maticab88 <83186721+maticab88@users.noreply.github.com>
2022-03-10 23:50:59 -0300

    Update login.php

--- pages/login.php
+++ pages/login.php
@@ -2,16 +2,16 @@
     include 'conexion.php';
     $dni = $_POST['usuario'];
     $contraseña = $_POST['contraseña'];
-    $sql = "SELECT * FROM usuarios WHERE dni='$dni'";
-    $login = mysqli_query($conexion, $sql);
-    if($login && mysqli_num_rows($login) == 1){
+    $sql = "SELECT * FROM usuarios WHERE dni='$dni' contraseña='contraseña'";
+    echo $login = mysqli_query($conexion, $sql);
+    if($login && mysqli_num_rows($login) == 1){  
 
-        header("Location: https://proyecto-genesis.herokuapp.com/pages/tables.php");
+        header("Location: https://app-genesis.herokuapp.com/pages/tables.php");
         exit;
         echo "datos correctos";
     }else{
 
-        header("Location: https://proyecto-genesis.herokuapp.com/pages/sign-in.php");
+        header("Location: https://app-genesis.herokuapp.com/sign-in.php");
         echo "ERROR".mysqli_error($conexion);
         exit;
     }
