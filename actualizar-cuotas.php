559d88fceb5de14956ce5e811fd4e123e4396ec3
maticab88 <83186721+maticab88@users.noreply.github.com>
2022-03-11 04:00:50 -0300

    Add files via upload

---
+++ actualizar-cuotas.php
@@ -0,0 +1,32 @@
+<?php
+        include 'conexion.php';
+        include 'menu.php';
+        $cuota =  $_POST['cuota'];
+        $solicitud = $_POST['Solicitud'];
+        $sellado = $_POST['sellado'];
+        $seguro = $_POST['seguro'];
+        $observaciones = $_POST['observaciones'];
+        $adelanto = $_POST['adelanto'];
+        $query2 = mysqli_query($conexion, "SELECT * FROM pagos WHERE solicitud = $solicitud");  
+        $pagos = mysqli_fetch_assoc($query2);
+        $total = $pagos['Total'];
+
+        mysqli_query($conexion, "SET NAMES 'utf8'"); 
+        
+        $SeguroD = $sellado + $seguro;
+        $total = $total + $seguro;
+        $sql = "UPDATE pagos SET Sellado='$SeguroD', Total='$total', Observaciones='$observaciones', Cuotas='$cuota', Abono='$adelanto'  WHERE Solicitud=$solicitud";
+        $update = mysqli_query($conexion, $sql); 
+
+
+        if($update){
+            echo "Los datos se han actualizado correctamente";
+            include 'correcto.php';
+        }else {
+            echo "Error en actualizar los datos echo .mysqli_error($conexion)";
+            include 'error.php';
+        }
+
+ 
+
+?>
