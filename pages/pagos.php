b06f9f79e1104de159914419a6062a720e5a6b78
maticab88 <83186721+maticab88@users.noreply.github.com>
2022-03-11 06:25:30 -0300

    Update pagos.php

--- pages/pagos.php
+++ pages/pagos.php
@@ -26,7 +26,7 @@
           <div class="card my-4">
             <div class="card-header p-0 position-relative mt-n4 mx-3 z-index-2">
               <div class="bg-primary shadow-primary border-radius-lg pt-4 pb-3">
-                <?php echo '<h6 class="text-white text-capitalize ps-3">'.$cliente['nombre'].'</h6>'; ?>
+                <h6 class="text-white text-capitalize ps-3"><?php echo $cliente['nombre']; ?></h6>
               </div>
             </div>
             <div class="card-body px-0 pb-2">
